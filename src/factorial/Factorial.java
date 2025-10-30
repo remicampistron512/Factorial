5c19648f36c65bd4f3af0b13876db4bdf7bcc91a
Rémi Campistron <remi.campistron@gmail.com>
2025-10-30 15:37:28 +0100

    Ajout de commentaires

--- src/factorial/Factorial.java
+++ src/factorial/Factorial.java
@@ -1,11 +1,16 @@
 package factorial;
 
 public class Factorial {
-
+	
 	public static void main(String[] args) {
-		System.out.println(factorial(5));
+		System.out.println(factorial(4));
 	}
 	
+	/** 
+	 *  Méthode qui met en œuvre la notion de factorielle telle que n ! = n*(n-1) …*
+	 * @param n
+	 * @return
+	 */
 	public static int  factorial(int n) {
 		if(n == 0) {
 			return 1;
